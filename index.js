3b67fcade5fc7bf1d6102b9d2decf08a4fdeab3d
Patt0lino <investmentmoises@gmail.com>
2022-09-16 04:00:26 -0300

    form de criacao

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
-const {engine} = require('express-handlebars')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
 //configs
+app.use(express.static('./public'))
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars' )
+
 app.use(express.json())
 
-app.use(express.static('./public'))
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.use(express.urlencoded({
+    extended:true
+}))
 
-
+//public routes
 app.get('/', (req, res)=>{
-
     res.render('home')
 })
+
+//cadastro
+app.get('/create', (req,res)=>{
+    res.render('create')
+})
+
+app.post('/create', (req,res)=>{
+
+    let body = req.body
+
+    console.log(body)
+
+    
+    res.redirect('/')
+
+   
+})
+
+//login
+
+
+
+
+//private routes
+// ver livros disponiveis 
+//editar livros
+//deletar livros
+
+
 
 
 
